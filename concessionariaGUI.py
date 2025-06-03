02e4b4c3f271ecc7ab4577f12e5a02dd3fa50bd5
Rodolfo Klippel <dolforr@gmail.com>
2025-06-03 00:40:32 -0300

    concessionariaGUI.py

--- concessionariaGUI.py
+++ concessionariaGUI.py
@@ -1,13 +1,8 @@
 import sys
-import datetime
-import random
-import time
-from datetime import datetime
-from datetime import timedelta
 from PyQt5.QtWidgets import QApplication, QMainWindow,QLineEdit
 from PyQt5.QtCore import QTimer,Qt
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QPushButton,QGridLayout,QWidget,QVBoxLayout
+from PyQt5.QtWidgets import QPushButton,QWidget,QVBoxLayout
 
 class MainWindow(QWidget):
 	def __init__(self):
@@ -21,7 +16,8 @@ class MainWindow(QWidget):
 		self.botao4 = QPushButton("SAIR",self)
 		self.botao6 = QPushButton("VOLTAR",self)
 		self.line_edit = QLineEdit(self)
-		self.botaoLE = QPushButton("SUBMIT",self)
+		self.botaoLEC = QPushButton("SUBMIT",self)
+		self.botaoLEV = QPushButton("SUBMIT",self)
 		self.comp = QLabel("Que carro você gostaria de comprar?",self)
 		self.vend = QLabel("Qual o nome do carro que você deseja vender?",self)
 		self.planilha_direta = QLabel("",self)
@@ -29,7 +25,8 @@ class MainWindow(QWidget):
 		self.botao_nao = QPushButton("NÃO",self)
 
 		self.line_edit.hide()
-		self.botaoLE.hide()
+		self.botaoLEC.hide()
+		self.botaoLEV.hide()
 		self.comp.hide()
 		self.vend.hide()
 		self.planilha_direta.hide()
@@ -42,7 +39,15 @@ class MainWindow(QWidget):
         [1,1,1,1,1]]
 		self.caixa=15000
 		
-		# self.botao_sim.clicked.connect(self.carro_comprado)
+		self.botao1.clicked.connect(self.compra)
+		self.botao2.clicked.connect(self.venda)
+		self.botao3.clicked.connect(self.aluguel)
+		self.botao5.clicked.connect(self.planilha)
+		self.botao_sim.clicked.connect(self.carro_comprado)
+		self.botao_nao.clicked.connect(self.aparece)
+		self.botaoLEC.clicked.connect(self.compra_decisao)
+		self.botaoLEV.clicked.connect(self.venda_valor)
+		self.botao6.clicked.connect(self.aparece)
 
 		self.initUI()
 
@@ -58,7 +63,8 @@ class MainWindow(QWidget):
 		self.vbox.addWidget(self.vend)
 		self.vbox.addWidget(self.comp)
 		self.vbox.addWidget(self.line_edit)
-		self.vbox.addWidget(self.botaoLE)
+		self.vbox.addWidget(self.botaoLEC)
+		self.vbox.addWidget(self.botaoLEV)
 		self.vbox.addWidget(self.planilha_direta)
 		self.vbox.addWidget(self.botao6)
 		self.vbox.addWidget(self.botao_sim)
@@ -79,43 +85,29 @@ class MainWindow(QWidget):
 					font-family:Comic Sans MS;
 					}
 				""")
-		self.botao1.clicked.connect(self.compra)
-		self.botao2.clicked.connect(self.venda)
-		self.botao3.clicked.connect(self.aluguel)
-		self.botao5.clicked.connect(self.planilha)
 	
 	def compra(self):
 		self.some()
 
 		self.comp.show()
 		self.line_edit.show()
-		self.botaoLE.show()
+		self.botaoLEC.show()
 
 		self.setLayout(self.vbox)
 
 		self.comp.setAlignment(Qt.AlignCenter)
-
-		self.botaoLE.clicked.connect(self.compra_decisao)
 		
 	def compra_decisao(self):
 		self.carroComp = self.line_edit.text().upper()
+		self.line_edit.setText("")
 		for i in range(0,len(self.lista[0])):
 			if self.carroComp == self.lista[0][i]:
 				self.comp.setText(f"O carro irá custar: {self.lista[1][i]*1.31}\nVocê aceita?")
 				break
 		self.line_edit.hide()
-		self.botaoLE.hide()
+		self.botaoLEC.hide()
 		self.botao_sim.show()
 		self.botao_nao.show()
-		
-		self.botao_nao.clicked.connect(self.aparece)
-		
-		try:
-			self.botao_sim.clicked.disconnect()
-		except TypeError:
-			pass
-		
-		self.botao_sim.clicked.connect(self.carro_comprado)
 		
 	def carro_comprado(self):
 		self.caixa+=self.lista[1][self.lista[0].index(self.carroComp)]*1.31
@@ -132,13 +124,20 @@ class MainWindow(QWidget):
 
 		self.vend.show()
 		self.line_edit.show()
-		self.botaoLE.show()
+		self.botaoLEV.show()
 
 		self.setLayout(self.vbox)
 
 		self.vend.setAlignment(Qt.AlignCenter)
-
-		self.botaoLE.clicked.connect(self.aparece)
+		
+	def venda_valor(self):
+		self.carroVend = self.line_edit.text().upper()
+		self.vend.setText(f"Por quanto você deseja vender o(a) {self.carroVend}?")
+		self.line_edit.setText("")
+		
+	
+	def venda_decisao(self):
+		pass
 
 	def aluguel(self):
 		pass
@@ -156,8 +155,6 @@ class MainWindow(QWidget):
 		
 		self.planilha_direta.setText(f"{self.texto_planilha}")
 		
-		self.botao6.clicked.connect(self.aparece)
-		
 	def some(self):
 		self.Texto.hide()
 		self.botao1.hide()
@@ -170,7 +167,8 @@ class MainWindow(QWidget):
 		self.vend.hide()
 		self.comp.hide()
 		self.line_edit.hide()
-		self.botaoLE.hide()
+		self.botaoLEC.hide()
+		self.botaoLEV.hide()
 		self.planilha_direta.hide()
 		self.botao6.hide()
 		self.botao_nao.hide()
